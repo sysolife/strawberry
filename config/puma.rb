a25ee0fe312eb4231b27a0ef96ba84288a0f3fcd
李剑 <jian.li@classiclaw.com>
2019-05-28 17:46:12 +0800

    增加puma.sh文件

--- config/puma.rb
+++ config/puma.rb
@@ -16,7 +16,7 @@ directory "#{application_path}/current"
 pidfile "#{application_path}/shared/tmp/pids/puma.pid"
 state_path "#{application_path}/shared/tmp/sockets/puma.state"
 stdout_redirect "#{application_path}/shared/log/puma.stdout.log", "#{application_path}/shared/log/puma.stderr.log"
-bind "unix://#{application_path}/shared/tmp/sockets/#{app_name}.sock"
+bind "unix:///#{application_path}/shared/tmp/sockets/#{app_name}.sock"
 activate_control_app "unix://#{application_path}/shared/tmp/sockets/pumactl.sock"
 
 #后台运行
